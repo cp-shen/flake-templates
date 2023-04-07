187a09ff5678748a786f33b46861773afce70546
scp <scp@archlinuxscp.xyz>
2023-04-07 17:54:11 +0800

    add haskell template

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
         path = ./templates/scala;
         description = "a nix flake template for scala dev environment";
       };
+      haskell = {
+        path = ./templates/haskell;
+        description = "a nix flake template for haskell dev environment";
+      };
     };
   };
 }
