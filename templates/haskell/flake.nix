d36cfbdbcd7e2ed6ab128ca06f207c915b49cb0d
scp <scp@archlinuxscp.xyz>
2023-04-22 20:24:13 +0800

    add stack support

--- templates/haskell/flake.nix
+++ templates/haskell/flake.nix
@@ -1,3 +1,7 @@
+# References:
+#   https://docs.haskellstack.org/en/stable/nix_integration/
+#   https://nixos.wiki/wiki/Haskell
+
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
@@ -8,47 +12,72 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        hl = pkgs.haskell.lib.compose;
-        haskellPackages = pkgs.haskellPackages;
+        ghcVersion = "ghc927";
+        hpkgs = pkgs.haskell.packages.${ghcVersion};
+        hlib = pkgs.haskell.lib.compose;
         trv = nixpkgs.lib.trivial;
         root = ./.;
-        project = extraModifiers: returnShellEnv:
-          haskellPackages.developPackage {
-            inherit root;
-            returnShellEnv = returnShellEnv;
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack";
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --no-nix \
+                --system-ghc \
+                --no-install-ghc \
+              "
+          '';
+        };
+        myDevTools = with hpkgs; [
+          ghc
+          stack-wrapped
+          cabal-install
+          haskell-language-server
+          implicit-hie
+          hlint
+          hpack
+          hoogle
+        ];
+        myNativeDeps = with pkgs; [ zlib ];
+        myProject = returnShellEnv: extraModifiers:
+          hpkgs.developPackage {
+            inherit root returnShellEnv;
             modifier = (trv.flip trv.pipe) ([
-              hl.enableStaticLibraries
-              hl.justStaticExecutables
-              hl.disableExecutableProfiling
+              hlib.enableStaticLibraries
+              hlib.justStaticExecutables
+              hlib.disableExecutableProfiling
+              (hlib.addBuildTools myNativeDeps)
             ] ++ extraModifiers);
           };
-      in {
-        packages.default = project [ ] false;
-        devShell = project [
-          (hl.addBuildTools (with haskellPackages; [
-            stack
-            cabal-install
-            haskell-language-server
-            implicit-hie
-            hlint
-            hpack
-          ]))
-          (hl.overrideCabal (old: {
+      in rec {
+        packages.default = myProject false [ ];
+        devShells.cabal2nix = myProject true [
+          (hlib.addBuildTools myDevTools)
+          (hlib.overrideCabal (old: {
             shellHook = (old.shellHook or "") + ''
               echo "Generating .cabal file from package.yaml using hpack, \
               remember to regenerate if you change package.yaml! \
               Although nix build dont care about it, \
-              its mostly for helping haskell-language-server." | ${pkgs.cowsay}/bin/cowsay
+              its mostly for helping haskell-language-server." | \
+              ${pkgs.cowsay}/bin/cowsay
 
               hpack
 
               export CABAL_CONFIG="$(pwd)/cabal_config"
             '';
           }))
-        ] true;
+        ];
+        # devShells.stack = pkgs.mkShell {
+        #   buildInputs = myDevTools ++ myNativeDeps;
+        #   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myNativeDeps;
+        # };
+        devShells.default = devShells.cabal2nix;
       }) // {
-        overlay = final: prev: {
-           # p_name_here = self.packages.${final.system}.default;
-        };
+        overlays.default = final: prev:
+          {
+            # package_name = self.packages.${final.system}.default;
+          };
       };
 }
